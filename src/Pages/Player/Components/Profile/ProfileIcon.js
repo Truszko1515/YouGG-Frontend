1d46da21af700b0c18ccd82324bfc56853d67078
Bartosz Truszkowski <truszkoo@gmail.com>
2024-08-22 01:32:46 +0200

    minor changes

--- src/Pages/Player/Components/Profile/ProfileIcon.js
+++ src/Pages/Player/Components/Profile/ProfileIcon.js
@@ -7,67 +7,62 @@ export default function ProfileIcon({
   summonerLevel,
   ProfileIconId,
   summonerTagLine,
-  // 
+  //
   tier,
   rank,
   leaguePoints,
   wins,
   losses,
 }) {
-
-  
   return (
     <>
       <div className="body">
-        <div className={styles.profileIcon}>
-          <div className={styles.imgTextWrapper}>
-
-            <div className={RankIconStyles.profileContainer}>
-              <img
-                src={process.env.REACT_APP_RANK_ICON_PATH + "TFT_Regalia_" + 
-                     tier.charAt(0).toUpperCase() + tier.slice(1).toLowerCase() + ".png"}
-                alt="Rank Icon"
-                className={RankIconStyles.rankIcon}
-              />
-              <div className={RankIconStyles.profileInfo}>
-                <h3 className={RankIconStyles.rankType}>Ranked Solo</h3>
-                <p className={RankIconStyles.summonerName}>{summonerName}</p>
-                <p className={RankIconStyles.rankDetails}>{tier} {rank}</p>
-                <p className={RankIconStyles.rankStats}>{leaguePoints} LP / {wins}W {losses}L</p>
-                <p className={RankIconStyles.winRatio}>Win Ratio {Math.round(wins/(wins+losses)*100)}%</p>
+        <div className={styles.container}>
+          <div className={RankIconStyles.profileContainer}>
+            <img
+              src={
+                process.env.REACT_APP_RANK_ICON_PATH +
+                "TFT_Regalia_" +
+                tier.charAt(0).toUpperCase() +
+                tier.slice(1).toLowerCase() +
+                ".png"
+              }
+              alt="Rank Icon"
+              className={RankIconStyles.rankIcon}
+            />
+            <div className={RankIconStyles.profileInfo}>
+              <h3 className={RankIconStyles.rankType}>Ranked Solo</h3>
+              <p className={RankIconStyles.summonerName}>{summonerName}</p>
+              <p className={RankIconStyles.rankDetails}>
+                {tier} {rank}
+              </p>
+              <p className={RankIconStyles.rankStats}>
+                {leaguePoints} LP / {wins}W {losses}L
+              </p>
+              <p className={RankIconStyles.winRatio}>
+                Win Ratio {Math.round((wins / (wins + losses)) * 100)}%
+              </p>
+            </div>
+          </div>
+          <div className={styles.profileIcon}>
+            <div className={styles.imgTextWrapper}>
+              <div className={styles.imgWrapper}>
+                <img
+                  src={
+                    process.env.REACT_APP_PROFILE_ICON_PATH +
+                    ProfileIconId +
+                    ".png"
+                  }
+                  className={styles.imageStyle}
+                  alt="Profile Icon"
+                />
+                <div className={styles.textOnImage}>{summonerLevel}</div>
+                {`${summonerTagLine}`}
               </div>
             </div>
-
-            <div className={styles.imgWrapper}>
-              <img
-                src={
-                  process.env.REACT_APP_PROFILE_ICON_PATH + ProfileIconId + ".png"
-                }
-                className={styles.imageStyle}
-              ></img>
-              <div className={styles.summonerName}>
-                {summonerName + summonerTagLine}
-              </div>
-              <div className={styles.textOnImage}>{summonerLevel} </div>
-            </div>
-
           </div>
         </div>
       </div>
     </>
   );
 }
-
-// useEffect(() => {
-//   const fetchData = async () => {
-//     const response = await fetch(
-//       "https://localhost:7041/api/summoner/" + summonerName
-//     );
-//     const data = await response.json();
-//     setSummonerInfo(data);
-//     setIsLoaded(true);
-//     console.log("Profile Icon - pobieranie ikonki")
-//   };
-
-//   fetchData();
-// }, [state]);
